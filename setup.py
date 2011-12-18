6c09ada9381c7d7fdd64e50d102110660bad7352
Sam Rushing <gitsam@rushing.nightmare.com>
2011-12-18 13:17:08 -0800

    first try at a pip-compatible setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- Mode: Python -*-
+
+from setuptools import setup, find_packages
+setup (
+    name = 'amqp_shrapnel',
+    version = '0.1',
+    packages = find_packages(),
+    author = 'Sam Rushing',
+    description = "AMQP for Shrapnel",
+    license = "Simplified BSD",
+    keywords = "amqp shrapnel",
+    url = "http://github.com/samrushing/amqp-shrapnel/tarball/master#egg=amqp-shrapnel-0.1",
+    dependency_links = ['http://github.com/ironport/shrapnel/master#egg=shrapnel-0.1']
+    )
