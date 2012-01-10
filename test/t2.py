8b23c13f06db2355dfdf40c17c70296daa727c15
Sam Rushing <gitsam@rushing.nightmare.com>
2012-01-10 12:54:13 -0800

    amqp-shrapnel version of test publisher

---
+++ test/t2.py
@@ -0,0 +1,17 @@
+# -*- Mode: Python -*-
+# companion to t0.py that uses amqp-shrapnel
+
+import amqp_shrapnel
+
+def t2():
+    global c
+    c = amqp_shrapnel.client (('guest', 'guest'), '127.0.0.1')
+    c.go() # i.e., connect...
+    ch = c.channel()
+    ch.basic_publish ('howdy there!', exchange='ething', routing_key='notification')
+    print 'published!'
+    coro.set_exit()
+
+import coro
+coro.spawn (t2)
+coro.event_loop()
