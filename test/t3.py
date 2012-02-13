100a96517f453b01fdfe02791864236375a1de90
Sam Rushing <gitsam@rushing.nightmare.com>
2012-02-13 13:00:12 -0800

    demo/test the 'publisher confirms' rabbitmq extension

---
+++ test/t3.py
@@ -0,0 +1,27 @@
+# -*- Mode: Python -*-
+# companion to t0.py that uses amqp-shrapnel
+# ... and uses rabbitmq's 'confirm' extension
+
+import amqp_shrapnel
+
+def t3():
+    global c
+    c = amqp_shrapnel.client (('guest', 'guest'), '127.0.0.1')
+    print 'connecting...'
+    c.go() # i.e., connect...
+    print 'channel...'
+    ch = c.channel()
+    print 'confirm_select...'
+    ch.confirm_select()
+    print 'entering send loop'
+    for i in range (10):
+        ch.basic_publish ('howdy there!', exchange='ething', routing_key='notification')
+        print 'sent'
+        ch.get_ack()
+        print 'confirmed'
+        coro.sleep_relative (1)
+    coro.set_exit()
+
+import coro
+coro.spawn (t3)
+coro.event_loop()
