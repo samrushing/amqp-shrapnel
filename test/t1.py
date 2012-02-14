51fc4e552597415aa073a4a86ded442e119be0c0
Sam Rushing <gitsam@rushing.nightmare.com>
2012-02-13 21:06:23 -0800

    throw in some properties to test the new property code

--- test/t1.py
+++ test/t1.py
@@ -3,5 +3,5 @@
 from amqplib import client_0_8 as amqp
 conn = amqp.Connection (userid='guest', password='guest', host='localhost', virtual_host='/')
 ch = conn.channel()
-msg = amqp.Message ('howdy there!')
+msg = amqp.Message ('howdy there!', content_type='text/fnord')
 ch.basic_publish (msg, 'ething', 'notification')
