96b18fa39df0e6b1ee1b5349635959e433a69712
Sam Rushing <gitsam@rushing.nightmare.com>
2012-02-15 17:30:15 -0800

    trying to up the version number for pip magic

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from setuptools import setup, find_packages
 setup (
     name              = 'amqp_shrapnel',
-    version           = '0.1',
+    version           = '0.5',
     packages          = find_packages(),
     author            = 'Sam Rushing',
     description       = "AMQP for Shrapnel",
     license           = "Simplified BSD",
     keywords          = "amqp shrapnel",
     url               = 'http://github.com/samrushing/amqp-shrapnel/',
-    download_url      = "http://github.com/samrushing/amqp-shrapnel/tarball/master#egg=amqp-shrapnel-0.1",
+    download_url      = "http://github.com/samrushing/amqp-shrapnel/tarball/master#egg=amqp-shrapnel-0.5",
     install_requires  = ['coro'],
     dependency_links  = ['http://github.com/ironport/shrapnel/tarball/master#egg=coro-0.1'],
     zip_safe          = False
